e82765c8042421e99cee93d992e8141fa449bd40
Ruoxi Wang <thuwrx10@gmail.com>
2018-05-15 16:16:31 +0800

    Adapted for FANUC 0i-TF control.

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ roughFeedRate = 600;
 roughZOffsets = [10 5 0];
 blankDia = 25;                                  % Blank material diameter
 teethToolNumber = '0606';
-teethToolDia = 3.175;
+teethToolDia = 3.175; % Useless for now
 teethFeedRate = 2000;
 teethSpindleSpeed = 5000;
 roughYCleariance = 5;
@@ -14,7 +14,7 @@ roughXCoords = -1;
 
 teethXRange = roughXCoords + 1.1 * [1 -1];
 
-gCodePrefix = 'M05\nG97\nM25\nCM 0.0\nG98\n';
+gCodePrefix = 'M05\nM25\nCM 0.0\nG98\n';
 
 if regen
     [ ...
@@ -27,13 +27,13 @@ if regen
         ] = generation_v2('fp.txt', blankDia);
 end
 
-roughingCode('O4502', leftRoughingToolPath, leftRoughingToolPathExtra, "%%\nO4502\n" + gCodePrefix, ...
+roughingCode('O4591.nc', leftRoughingToolPath, leftRoughingToolPathExtra, "%%\nO4591\n" + gCodePrefix, ...
     roughToolNumber, roughSpindleSpeed, zSafe, roughZOffsets, roughXCoords, blankDia, roughYCleariance, roughFeedRate);
-roughingCode('O4504', rightRoughingToolPath, rightRoughingToolPathExtra, "%%\nO4504\n" + gCodePrefix, ...
+roughingCode('O4592.nc', rightRoughingToolPath, rightRoughingToolPathExtra, "%%\nO4592\n" + gCodePrefix, ...
     roughToolNumber, roughSpindleSpeed, zSafe, roughZOffsets, roughXCoords, blankDia, roughYCleariance, roughFeedRate);
-teethCode('O4503', leftTeethToolPath, "%%\nO4503\n" + gCodePrefix, teethToolNumber, ...
+teethCode('O4593.nc', leftTeethToolPath, "%%\nO4593\n" + gCodePrefix, teethToolNumber, ...
     teethSpindleSpeed, zSafe, teethXRange, teethFeedRate, teethToolDia);
-teethCode('O4505', rightTeethToolPath, "%%\nO4505\n" + gCodePrefix, teethToolNumber, ...
+teethCode('O4594.nc', rightTeethToolPath, "%%\nO4594\n" + gCodePrefix, teethToolNumber, ...
     teethSpindleSpeed, zSafe, teethXRange, teethFeedRate, teethToolDia);
 
 function roughingCode ( filename, toolPath, toolPathExtra, gCodePrefix, toolNumber, ...
@@ -44,38 +44,38 @@ function roughingCode ( filename, toolPath, toolPathExtra, gCodePrefix, toolNumb
     fprintf(f, 'T%s\n', toolNumber);
     fprintf(f, 'S%d M203\nM8\n', spindleSpeed);
     fprintf(f, gCodePrefix2);
-    fprintf(f, 'G1 X%.3f F8000\n', zSafe);
+    fprintf(f, 'G0 X%.3f\n', zSafe);
 
     for i = 1:length(xCoords)
         for zOffset = zOffsets
             [YZ, A] = toMachineRef(toolPath{1});
-            fprintf(f, 'G1 Z%.3f Y%.3f X%.3f %s F8000\n', ...
+            fprintf(f, 'G0 Z%.3f Y%.3f X%.3f %s\n', ...
                 xCoords(i), ...
                 blankDia/2+yCleariance, ...
                 zSafe + zOffset, ...
                 ACoord(A, true));
-            fprintf(f, 'G1 X%.3f F8000\n', YZ(2) + zOffset);
+            fprintf(f, 'G0 X%.3f\n', YZ(2) + zOffset);
             %fprintf(f, 'G93\n');                        % Inverse time feed rate mode
             fprintf(f, 'G1 Y%.3f F%.3f\n', YZ(1), feedRate);%/(YZ(1) + blankDia/2 + yCleariance));
 
             for j = 2:length(toolPath)
                 [YZ, A] = toMachineRef(toolPath{j});
-                fprintf(f, 'Y%.3f X%.3f %s F%.3f\n', ...
-                    YZ(1), YZ(2) + zOffset, ACoord(A), feedRate);%/equvDist(toolPath{j-1}, toolPath{j}, blankDia/2));
+                fprintf(f, 'Y%.3f X%.3f %s\n', ...
+                    YZ(1), YZ(2) + zOffset, ACoord(A));%/equvDist(toolPath{j-1}, toolPath{j}, blankDia/2));
             end
             %fprintf(f, 'G94\n');                        % Inverse time feed rate mode
 
-            fprintf(f, 'G1 X%.3f F8000\n', zSafe + zOffset);
+            fprintf(f, 'G0 X%.3f\n', zSafe + zOffset);
 
             for j = [1 3]
                 [YZ, A] = toMachineRef(toolPathExtra(j:j+1,:));
-                fprintf(f, 'G1 Y%.3f X%.3f %s F8000\n', ...
+                fprintf(f, 'G0 Y%.3f X%.3f %s\n', ...
                     (blankDia/2 + yCleariance) * sign(YZ(1)), ...
                     zSafe + zOffset, ACoord(A, true));
                 fprintf(f, 'X%.3f\n', YZ(2) + zOffset);
-                fprintf(f, 'G1 Y%.3f F%.3f\n', ...
-                    YZ(1), feedRate);
-                fprintf(f, 'G1 X%.3f F8000\n', zSafe + zOffset);
+                fprintf(f, 'G1 Y%.3f\n', ...
+                    YZ(1));
+                fprintf(f, 'G0 X%.3f\n', zSafe + zOffset);
             end
         end
     end
@@ -87,11 +87,11 @@ function teethCode ( filename, toolPath, gCodePrefix, toolNumber, ...
        spindleSpeed, zSafe, xRange, feedRate, toolDia )
     f = fopen(filename, 'w');
     fprintf(f, gCodePrefix);
-    fprintf(f, 'T%s\n', toolNumber);
+    %fprintf(f, 'T%s\n', toolNumber);
     fprintf(f, 'S%d M203\n', spindleSpeed);
     gCodePrefix2 = '';
     fprintf(f, gCodePrefix2);
-    fprintf(f, 'G1 X%.3f F8000\n', zSafe);
+    fprintf(f, 'G0 X%.3f\n', zSafe);
 
     for idxPass = 1:length(toolPath)
         if idxPass == length(toolPath)
@@ -100,22 +100,23 @@ function teethCode ( filename, toolPath, gCodePrefix, toolNumber, ...
 
         for idxTooth = 1:length(toolPath{idxPass})
             [YZ, A] = toMachineRef(toolPath{idxPass}{idxTooth}{1});
-            fprintf(f, 'G1 Z%.3f Y%.3f X%.3f %s F8000\n', ...
+            fprintf(f, 'G0 Z%.3f Y%.3f X%.3f %s \n', ...
                 xRange(1), ...
                 YZ(1), ...
                 zSafe, ...
                 ACoord(A, true));
-            fprintf(f, 'G1 X%.3f F8000\n', YZ(2) + 1);
+            fprintf(f, 'G0 X%.3f\n', YZ(2) + 1);
+            fprintf(f, 'F%.3f\n', feedRate);
 
             for idxCut = 1:length(toolPath{idxPass}{idxTooth})
                 [YZ, A] = toMachineRef(toolPath{idxPass}{idxTooth}{idxCut});
-                fprintf(f, 'G1 Z%.3f Y%.3f X%.3f %s F600.0\n', ...
+                fprintf(f, 'G1 Z%.3f Y%.3f X%.3f %s\n', ...
                     xRange(1), YZ(1), YZ(2), ACoord(A));
-                fprintf(f, 'Z%.3f F%.3f\n', xRange(2), feedRate);
+                fprintf(f, 'Z%.3f\n', xRange(2));
                 if idxCut ~= length(toolPath{idxPass}{idxTooth})
-                    fprintf(f, 'G1 X%.3f F8000\nZ%.3f\n', YZ(2) + 0.2, xRange(1));
+                    fprintf(f, 'G0 X%.3f\nZ%.3f\n', YZ(2) + 0.2, xRange(1));
                 else
-                    fprintf(f, 'G1 X%.3f F8000\n', zSafe);
+                    fprintf(f, 'G0 X%.3f\n', zSafe);
                 end
             end
         end
